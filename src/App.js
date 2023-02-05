ffb20317ded292904a74defb833161af52055f66
Oxana <oavoskresenskaya@mail.ru>
2023-02-05 16:45:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
 function App() {
+  const [quote, setQuote] = useState("");
+  /*const getQuote = useCallback( async() => {
+    const responce = await fetch ('https://api.quotable.io/random');
+    const data = await responce.json();
+    setQuote(data.content);
+  })
+  useEffect (()=> {
+    getQuote() 
+  }, [getQuote])*/
+  useEffect (() =>{
+    const getQuote = async () => {
+      const responce = await fetch ('https://api.quotable.io/random')
+      const data = await responce.json()
+      console.log(data);
+      setQuote(data)
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{quote}</p>
     </div>
   );
 }
